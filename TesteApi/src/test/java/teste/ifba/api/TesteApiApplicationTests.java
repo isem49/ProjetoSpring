122ea6ef099afd95650fe4876bd72efda0cd0833
Júnior <56940605+isem49@users.noreply.github.com>
2022-08-16 07:39:03 -0300

    Add files via upload

---
+++ TesteApi/src/test/java/teste/ifba/api/TesteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.ifba.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
